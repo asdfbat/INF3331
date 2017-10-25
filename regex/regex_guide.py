b7479d1101c94878e3e3ebea214a031fdbc9587e
asdfbat <jonas.s.lunde@outlook.com>
2017-10-25 22:41:37 +0200

    none

--- regex/regex_guide.py
+++ regex/regex_guide.py
@@ -1,23 +1,19 @@
-import red
+import re
 
 s = "asdf ASDF AsDf asdfbat ASDFbat A$DFbat asdfbatasdfbat Adfbat bat asdf"
 
-### Substituting ###
-print(re.sub(r"asdf", "x", s))  # Use the r (raw) in fron of the string to tell python
-                                # the string should be interpreted as "raw",
-                                # and string-commands like \n, \b, etc won't be executed.
+regex = r"asdf" # Use the r (raw) in fron of the string to tell python the string
+# should be interpreted as "raw" and string-commands like \n, \b, etc won't be executedself
 
 ### Finding stuff ###
-print(re.findall(r"as.fbat", s))
+matches = re.findall(regex, s)  # Returns a list of all mathces in string
 
+### Substituting ###
+new_s = re.sub(regex, "x", s)  # Returns a new string with all matching values substituted
 
-### Custom character class [ ] ###
-# We can use a custom character class to further specify allowed characters.
-# It counts as a single character, and specifies the allowed values.
-regex = "[aA][sS].."   # a or A, followed by s or S, then two other characters, is allowed
-print(re.findall(regex, s))
-
-# We can also specify intervals:
-regex = "[0-9]"
-regex = "[A-F]"
-regex = "[a-f]"
+# NOTES:
+# In the replace section, most RegEx syntax doesn't apply, so characters
+# like | $ { } etc doesn't need to be cancelled:
+re.sub("\[\]", "[]", s)
+# Back-refrences can be made both in the search, and the replace section:
+re.sub("([A-C])\1", "\1", s)
